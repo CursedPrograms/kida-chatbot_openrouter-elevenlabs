c4ab944980806a644a5da0418ea25927468bbc4e
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2025-05-04 15:12:10 +0200

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,15 +1,5 @@
 @echo off
-
-set "VENV_DIR=psdenv"
-
-rem 
-if not exist "%VENV_DIR%" (
-    rem 
-    python -m venv "%VENV_DIR%"
-)
-
-rem 
-call "%VENV_DIR%\Scripts\activate" && python main.py
-
-rem 
+cd /d "C:\Users\Cursed\Desktop\voiceAI"
+echo Running voiceAI...
+python kida-chatbot.py
 pause
